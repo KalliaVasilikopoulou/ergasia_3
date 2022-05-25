cdc46496510d615c12b8b2db900a9b2f71a509ef
KalliaVasilikopoulou <98029855+KalliaVasilikopoulou@users.noreply.github.com>
2022-05-25 23:36:15 +0300

    project ex3
    To run it you need to install eclipse for java developers. Run the Application.java class and go to http://localhost:8080/swagger-ui/.

---
+++ gr.upatras.ergasia3/src/main/java/gr/upatras/rest/example/Text.java
@@ -0,0 +1,23 @@
+package gr.upatras.rest.example;
+
+//this is the text that will be posted
+
+public class Text {
+	private String txt;
+	
+	public Text() {}
+
+	public Text(String txt) {
+		super();
+		this.txt = txt;
+	}
+
+	public String getTxt() {
+		return txt;
+	}
+
+	public void setTxt(String txt) {
+		this.txt = txt;
+	}
+	
+}
